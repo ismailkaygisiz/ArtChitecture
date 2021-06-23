21b52b451272683d1490cd3ca53def92946ccaed
ismailkaygisiz <kaygisizismail24@gmail.com>
2021-06-23 20:56:44 +0300

    Migration System Added

---
+++ DataAccess/Migrations/20210623172329_Initial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
