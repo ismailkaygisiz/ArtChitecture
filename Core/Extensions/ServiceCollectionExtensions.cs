9f4dce9923b351d12f0aa6a611fe46f7ccd5f817
ismailkaygisiz <kaygisizismail24@gmail.com>
2022-07-14 00:57:45 +0300

    Update ServiceCollectionExtensions.cs

--- Core/Extensions/ServiceCollectionExtensions.cs
+++ Core/Extensions/ServiceCollectionExtensions.cs
@@ -5,8 +5,7 @@ namespace Core.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddDependencyResolvers(this IServiceCollection serviceCollection,
-            params ICoreModule[] modules)
+        public static IServiceCollection AddDependencyResolvers(this IServiceCollection serviceCollection, params ICoreModule[] modules)
         {
             foreach (var module in modules) module.Load(serviceCollection);
 
